ffaac86dda2176577ab1597df477b0bb19edd1f0
Mauro Sérgio <maurosergiocantuaria@gmail.com>
2024-06-05 17:02:11 -0300

    placa

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Pages/Login';
-import FinalizarPedido from './Pages/FinalizarPedido'
+import ConsultarPlaca from './Pages/Placa'
 import Home from './Pages/Home';
 
 function Navigation() {
@@ -8,7 +8,7 @@ function Navigation() {
         <Router>
             <Routes>
                 <Route path="/" element={<Login />} />
-                <Route path="/FinalizarPedido" element={<FinalizarPedido/ >} />
+                <Route path="/ConsultarPlaca" element={<ConsultarPlaca />} />
                 <Route path="/home" element={<Home />} />
             </Routes>
         </Router>
